1fbb4a193bb80b4b60dde0071b2263ea92666c84
neorefraction <johnnyastudilloso@gmail.com>
2025-05-13 12:43:08 +0200

    Main layout created

--- app/src/main/java/com/neorefraction/htm1_aiassistant/MainActivity.kt
+++ app/src/main/java/com/neorefraction/htm1_aiassistant/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.neorefraction.htm1_aiassistant
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById<LinearLayout>(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
